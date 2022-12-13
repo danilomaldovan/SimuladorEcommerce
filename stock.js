98c32c64e4bf5fbb135cf73ab7415bbf66a2493a
danilomaldovan <115048860+danilomaldovan@users.noreply.github.com>
2022-12-12 22:02:03 -0300

    Add files via upload

---
+++ stock.js
@@ -0,0 +1,62 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Camperon",
+        tipo: "Campera",
+        descripcion: "Una campera bien caliente, bien Canalla",
+        precio: 20000,
+        talle: "L",
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "Remera",
+        tipo: "remera",
+        descripcion: "La remera de la ciudad",
+        precio: 5000,
+        talle: "L",
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "Camiseta",
+        tipo: "camisa",
+        descripcion: "Llevas la camiseta en la piel",
+        precio: 18000,
+        talle: "L",
+        cantidad:0
+    },
+
+    {
+        id: 4,
+        nombre: "Short",
+        tipo: "Short",
+        descripcion: "la mejor calidad, el mejor club",
+        precio: 8000,
+        talle: "M",
+        cantidad:0
+    },
+
+    {
+        id: 5,
+        nombre: "Gorra",
+        tipo: "gorra",
+        descripcion: "Miles de sentimientos en una gorra",
+        precio: 8000,
+        talle: "M",
+        cantidad:0
+    },
+
+    {
+        id: 6,
+        nombre: "Buzo",
+        tipo: "buzo",
+        descripcion: "El buzo de la pasion del pais",
+        precio: 12000,
+        talle: 40,
+        cantidad:0
+    }
+]
